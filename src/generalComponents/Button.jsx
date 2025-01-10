dcf1d950280ee2143513fedf31084dfbadbdd71a
AbroShahzeb <khizr77@gmail.com>
2025-01-10 15:06:04 +0500

    Deployment

--- src/generalComponents/Button.jsx
+++ src/generalComponents/Button.jsx
@@ -10,7 +10,7 @@ export const Button = ({
     primary:
       "py-250 px-300 rounded-8 border-2 border-nuetral-900 bg-light-salmon-50 flex items-center gap-200 text-sm font-semibold uppercase font-martian-mono hover:bg-gradient ",
     secondary:
-      "py-250 px-300 rounded-8 border-2 border-nuetral-0 bg-transparent flex items-center gap-200 text-sm font-semibold uppercase font-martian-mono text-neutral-0",
+      "py-250 px-300 rounded-8 border-2 border-nuetral-0 bg-transparent flex items-center gap-200 text-sm font-semibold uppercase font-martian-mono text-neutral-0 heh",
   };
   return (
     <button
